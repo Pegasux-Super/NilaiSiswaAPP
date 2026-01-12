f59551e69e442288009e8dcf4a1488ba0cce2529
Pegasux-Super <azzammuzzacky5@gmail.com>
2026-01-12 09:47:00 +0700

    Tambah input nilai & nama

--- NilaiSiswaAPP/Program.cs
+++ NilaiSiswaAPP/Program.cs
@@ -1,5 +1,10 @@
 ﻿int nilai;
-string namaa;
+string nama;
 
-Console.WriteLine("Aplikasi Nilai Siswa"); 
+Console.WriteLine("Aplikasi Nilai Siswa");
 
+Console.Write("input Nama: ");
+nama = Console.ReadLine();
+
+Console.Write("Input Nilai siswa: "); 
+nilai = int.Parse(Console.ReadLine()); 
